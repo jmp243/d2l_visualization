8d49f3db504fbe8d992e374f190dc4b4ba1012c3
Jung Mee Park <jmpark@arizona.edu>
2023-04-18 10:57:48 -0700

    added a new recode to all data frames

--- 01-read-in-data.R
+++ 01-read-in-data.R
@@ -44,6 +44,11 @@ list2env(my_sheets2, envir=.GlobalEnv)
   STRM == "2224" ~ "Fall 22",
   STRM == "2231" ~ "Spring 23"))
 
+`Behavior 2 Quizzes` <- `Behavior 2 Quizzes` %>% mutate(Term = case_when(
+  STRM == "2214" ~ "Fall 21",
+  STRM == "2221" ~ "Spring 22",
+  STRM == "2224" ~ "Fall 22",
+  STRM == "2231" ~ "Spring 23"))
 # mylist=list(df1,df2)
 # lapply(mylist,function(x){
 #   x$newVar=x$A1
@@ -56,9 +61,9 @@ my_dfs <- list(`Behavior 0`, `Behavior 1`, `Behavior 2 Assignments`,
                `Behavior 5 quizzes current sem`
 )
 
-mapped_df <- my_dfs |> 
+mapped_df <- my_dfs |>
   map(\(x) {
-    x |> 
+    x |>
       mutate(
         Term = case_when(
           STRM == "2214" ~ "Fall 21",
@@ -70,43 +75,67 @@ mapped_df <- my_dfs |>
       )
   })
 
-mapped_df %>% 
-  map_df(as_tibble)
+# mapped_df %>% 
+#   map_df(as_tibble)
 # my_dfs <- setNames(nm = my_dfs, c(`Behavior 0`, `Behavior 1`, `Behavior 2 Assignments`, 
 #                            `Behavior 2 Quizzes`, `Behavior 3`,
 #                            `Behavior 4 Assignments`, `Behavior 4 Quizzes`, `Behavior 4 Root Content Objects`,
 #                            `Behavior 5 Assign current sem`, `Behavior 5 Assignments`, `Behavior 5 Quizzes`,
 #                            `Behavior 5 quizzes current sem`))
-names(mapped_df) <- my_dfs
-
-list2env(mapped_df,envir = .GlobalEnv) # error
+# names(mapped_df) <- my_dfs
+# 
+# list2env(mapped_df,envir = .GlobalEnv) # error
 # do.call(rbind, lapply(mapped_df, as.data.frame))
-
-list2env(list = mapped_df(1 = `Behavior 0`, 2 = `Behavior 1`, 3 = `Behavior 2 Assignments`, 
-              4 = `Behavior 2 Quizzes`, 5 = `Behavior 3`, 6 = `Behavior 4 Assignments`,
-              7 = `Behavior 4 Quizzes`, 8 = `Behavior 4 Root Content Objects`, 9 = `Behavior 5 Assign current sem`,
-              10 = `Behavior 5 Assignments`, 11 = `Behavior 5 Quizzes`, 12 = `Behavior 5 quizzes current sem`), envir = .GlobalEnv)
+# 
+# list2env(list = mapped_df(1 = `Behavior 0`, 2 = `Behavior 1`, 3 = `Behavior 2 Assignments`, 
+#               4 = `Behavior 2 Quizzes`, 5 = `Behavior 3`, 6 = `Behavior 4 Assignments`,
+#               7 = `Behavior 4 Quizzes`, 8 = `Behavior 4 Root Content Objects`, 9 = `Behavior 5 Assign current sem`,
+#               10 = `Behavior 5 Assignments`, 11 = `Behavior 5 Quizzes`, 12 = `Behavior 5 quizzes current sem`), envir = .GlobalEnv)
 
 # names(my_dfs) <- c(`Behavior 0`, `Behavior 1`, `Behavior 2 Assignments`, 
 #                `Behavior 2 Quizzes`, `Behavior 3`,
 #                `Behavior 4 Assignments`, `Behavior 4 Quizzes`, `Behavior 4 Root Content Objects`,
 #                `Behavior 5 Assign current sem`, `Behavior 5 Assignments`, `Behavior 5 Quizzes`,
 #                `Behavior 5 quizzes current sem`)
+# 
+# setNames(nm = mapped_df, c(`Behavior 0`, `Behavior 1`, `Behavior 2 Assignments`, 
+#                            `Behavior 2 Quizzes`, `Behavior 3`,
+#                            `Behavior 4 Assignments`, `Behavior 4 Quizzes`, `Behavior 4 Root Content Objects`,
+#                            `Behavior 5 Assign current sem`, `Behavior 5 Assignments`, `Behavior 5 Quizzes`,
+#                            `Behavior 5 quizzes current sem`))
+# list2env(mapped_df, envir =.GlobalEnv)
+decoder <- tribble(
+  ~STRM, ~Term,
+  "2214", "Fall 21",
+  "2221", "Spring 22",
+  "2224", "Fall 22",
+  "2231", "Spring 23"
+)
 
-setNames(nm = mapped_df, c(`Behavior 0`, `Behavior 1`, `Behavior 2 Assignments`, 
-                           `Behavior 2 Quizzes`, `Behavior 3`,
-                           `Behavior 4 Assignments`, `Behavior 4 Quizzes`, `Behavior 4 Root Content Objects`,
-                           `Behavior 5 Assign current sem`, `Behavior 5 Assignments`, `Behavior 5 Quizzes`,
-                           `Behavior 5 quizzes current sem`))
-list2env(mapped_df, envir =.GlobalEnv)
+## 
+out <- map(my_dfs, \(x) dplyr::left_join(x, decoder))
+names(out) <- my_dfs
+
+
+# list2env(setNames(out, ls(pattern = "Behavior\\d\\d\\.csv")), envir = .GlobalEnv)
+# list2env(out, envir = .GlobalEnv)
+
+names(out) <- c("Behavior 0", "Behavior 1", "Behavior 2 Assignments", 
+                    "Behavior 2 Quizzes", "Behavior 3",
+                    "Behavior 4 Assignments", "Behavior 4 Quizzes", "Behavior 4 Root Content Objects",
+                    "Behavior 5 Assign current sem", "Behavior 5 Assignments", "Behavior 5 Quizzes",
+                    "Behavior 5 quizzes current sem")
+
+list2env(out,.GlobalEnv)
+
 # for (i in 1:length(mapped_df)) {
 #   region[i] <- as.data.frame(mapped_df[[i]])
 # }
 # list2env(setNames(df_list, ls(pattern = "Behavior\\d\\d\\")), envir = .GlobalEnv)
-
-names(mapped_df) <- mapped_df
-
-list2env(mapped_df, envir=.GlobalEnv)# n <- length(mapped_df[[14]])
+# 
+# names(mapped_df) <- mapped_df
+# 
+# list2env(mapped_df, envir=.GlobalEnv)# n <- length(mapped_df[[14]])
 # DF <- structure(mapped_df, row.names = c(NA, -n), class = "data.frame")
 
 # #method 1 to rename variables
@@ -134,20 +163,6 @@ my_function <- function(x) x %>%
     )
   )
 
-decoder <- tribble(
-  ~STRM, ~Term,
-  "2214", "Fall 21",
-  "2221", "Spring 22",
-  "2224", "Fall 22",
-  "2231", "Spring 23"
-)
-
-## 
-out <- map(my_dfs, \(x) dplyr::left_join(x, decoder))
-names(out) <- my_dfs
-
-# list2env(setNames(out, ls(pattern = "Behavior\\d\\d\\.csv")), envir = .GlobalEnv)
-list2env(out, envir = .GlobalEnv)
 
 # lapply(my_dfs, function(x) left_join(x, decoder))
 # 
@@ -159,9 +174,15 @@ lapply(my_dfs, my_function)
 
 # list2env(my_new_df, envir=.GlobalEnv)
 
+#### Behavior 2 quizzes ####
+`Behavior 2 Quizzes` %>% 
+  count(NO_QUIZ_DUEDATE)
+
+#### Save files to CSV ####
 write_csv(`Background Context`, "../data/background_context.csv")
 
 write_csv(`Background Context Course Level`, "../data/background_context_course_level.csv")
 
 write_csv(`Behavior 0`, "../data/cleaned_data/Behavior_0.csv")
 
+write_csv(`Behavior 2 Quizzes`, "../data/cleaned_data/Behavior_2_quizzes.csv")
